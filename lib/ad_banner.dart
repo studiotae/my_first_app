29cc405004d1ca68ab0bc34210fdae95ed038693
studiotae <studiotae874@gmail.com>
2026-02-25 18:53:56 +0900

    Change to production AdMob ID

--- lib/ad_banner.dart
+++ lib/ad_banner.dart
@@ -15,8 +15,8 @@ class _SimpleBannerAdState extends State<SimpleBannerAd> {
 
   // ★これで自動的に「Android」と「iOS」を判別してIDを切り替えます
   final String _adUnitId = Platform.isAndroid
-      ? 'ca-app-pub-3940256099942544/6300978111' // Androidテスト用
-      : 'ca-app-pub-3940256099942544/2934735716'; // iOSテスト用
+      ? 'ca-app-pub-9798347852135431/8804016579' // Android本番用
+      : 'ca-app-pub-9798347852135431/8804016579'; // iOS本番用（同じIDを使用）
 
   @override
   void initState() {
